5cc30dc55f3358b0184c92c531e2129c984afac0
ChristianTran <tikris@live.jp>
2020-01-03 15:00:37 +0100

    Suite tests critere

--- src/test/java/org/projet_selenium/TestCritere.java
+++ src/test/java/org/projet_selenium/TestCritere.java
@@ -49,18 +49,35 @@ public class TestCritere {
 		a.moveToElement(page_Accueil.sous_menu_critere).click().build().perform();	
 		PageCritere page_Critere = PageFactory.initElements(driver, PageCritere.class);
 		
-		//Test si on se trouve bien dans la page criteres
+		//Test si on se trouve bien sur la page critères
 		assertEquals("La page Critère n'a pas été affiché", "Types de critères Liste", page_Critere.text_page_criteres.getText());
 		
 		//Tests sur les noms des colonnes
-		assertEquals("Le tableau n'est pas bien affiché, la colonne Nom est manquante", "Nom", page_Critere.nom.getText());
-		assertEquals("Le tableau n'est pas bien affiché, la colonne Code est manquante", "Code", page_Critere.code.getText());
-		assertEquals("Le tableau n'est pas bien affiché, la colonne Type est manquante", "Type", page_Critere.type.getText());
-		assertEquals("Le tableau n'est pas bien affiché, la colonne Activé est manquante", "Activé", page_Critere.active.getText());
-		assertEquals("Le tableau n'est pas bien affiché, la colonne Opérations est manquante", "Opérations", page_Critere.operations.getText());
+		assertEquals("Le tableau n'est pas bien affiché, la colonne Nom est différente", "Nom", page_Critere.nom.getText());
+		assertEquals("Le tableau n'est pas bien affiché, la colonne Code est différente", "Code", page_Critere.code.getText());
+		assertEquals("Le tableau n'est pas bien affiché, la colonne Type est différente", "Type", page_Critere.type.getText());
+		assertEquals("Le tableau n'est pas bien affiché, la colonne Activé est différente", "Activé", page_Critere.active.getText());
+		assertEquals("Le tableau n'est pas bien affiché, la colonne Opérations est différente", "Opérations", page_Critere.operations.getText());
+		
+		assertTrue("Le tableau n'est pas bien affiché, la colonne Nom n'est pas affiché", page_Critere.nom.isDisplayed());
+		assertTrue("Le tableau n'est pas bien affiché, la colonne Code n'est pas affiché", page_Critere.code.isDisplayed());
+		assertTrue("Le tableau n'est pas bien affiché, la colonne Type n'est pas affiché", page_Critere.type.isDisplayed());
+		assertTrue("Le tableau n'est pas bien affiché, la colonne Activé n'est pas affiché", page_Critere.active.isDisplayed());
+		assertTrue("Le tableau n'est pas bien affiché, la colonne Opérations n'est pas affiché", page_Critere.operations.isDisplayed());
 		
 		//Test si le bouton créé est présent
 		assertTrue("Le bouton créer n'est pas présent", page_Critere.button_creer.isDisplayed());
+		
+		//Click sur le bouton créer
+		page_Critere.button_creer.click();
+		
+		//Tests si on se trouve bien sur la page de création de critères
+		assertEquals("La page de création de critère n'a pas été affiché","Créer Type de critère",page_Critere.text_page_criteres_creer.getText());
+		
+		//Tests pour voir si tous les boutons sont affichées
+		assertTrue("Le boutton Enregistrer ne s'est pas affiché", page_Critere.button_enregistrer.isDisplayed());
+		assertTrue("Le boutton Enregistrer ne s'est pas affiché", page_Critere.button_sauver_continuer.isDisplayed());
+		assertTrue("Le boutton Enregistrer ne s'est pas affiché", page_Critere.button_annuler.isDisplayed());
 		
 		Thread.sleep(pause);
 		
