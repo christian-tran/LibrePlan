fdb5c37f1b4e3d9bd7007db9fde7c0a483c149c9
AnneLab17 <anne.laborderie17@gmail.com>
2020-01-03 12:13:22 +0100

    Anne, modif TestRessources et PageRessources

--- src/main/java/org/projet_selenium/PageParticipants.java
+++ src/main/java/org/projet_selenium/PageParticipants.java
@@ -1,5 +1,32 @@
 package org.projet_selenium;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class PageParticipants extends PageAbstractBandeau {
+	
+	@FindBy (xpath = "//div[text()='Liste des participants']")
+	WebElement text_page_participants;
+	
+	@FindBy (xpath = "//table//div[@class='z-column-cnt'][text()='Surnom']")
+	WebElement surnom;
+	
+	@FindBy (xpath = "//table//div[@class='z-column-cnt'][text()='Prénom']")
+	WebElement prenom;
+	
+	@FindBy (xpath = "//table//div[@class='z-column-cnt'][text()='ID']")
+	WebElement id;
+	
+	@FindBy (xpath = "//table//div[@class='z-column-cnt'][text()='Code']")
+	WebElement code;
+	
+	@FindBy (xpath = "//table//div[@class='z-column-cnt'][text()='En file']")
+	WebElement en_file;
+	
+	@FindBy (xpath = "//table//div[@class='z-column-cnt'][text()='Opérations']")
+	WebElement operations;
+	
+	@FindBy (xpath = "//table//div[@class='z-column-cnt'][text()='Opérations']")
+	WebElement button_creer;
 
 }
--- src/test/java/org/projet_selenium/TestRessources.java
+++ src/test/java/org/projet_selenium/TestRessources.java
@@ -1,5 +1,6 @@
 package org.projet_selenium;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.concurrent.TimeUnit;
@@ -25,7 +26,7 @@ public class TestRessources {
 	public void setUp() throws Exception {
 		driver = OutilTechnique.choisirNavigateur(ENavigateur.chrome);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		BDDConnexion.insertData("src/test/JDD/JDD.xml");
+//		BDDConnexion.insertData("src/test/JDD/JDD.xml");
 		
 	}
 	
@@ -47,11 +48,24 @@ public class TestRessources {
 		//Test pour voir si le login s'est bien déroulé
 		assertTrue(page_Accueil.onglet_calendrier.isDisplayed());
 		
-		//Mouse-over
+		//Mouse-over et accès page Participants
 		Actions a = new Actions(driver);
 		a.moveToElement(page_Accueil.onglet_ressources).build().perform();
 		a.moveToElement(page_Accueil.sous_menu_participants).click().build().perform();	
 		PageParticipants page_Participants = PageFactory.initElements(driver, PageParticipants.class);
+		
+		//Test si on se trouve bien sur la page Participants
+		assertEquals("La page Participants n'a pas été affiché", "Liste des participants", page_Participants.text_page_participants.getText());
+		
+		//Tests sur les noms des colonnes
+		assertEquals("Le tableau n'est pas bien affiché, la colonne Nom est manquante", "Surnom", page_Participants.surnom.getText());
+		assertEquals("Le tableau n'est pas bien affiché, la colonne Prénom est manquante", "Prénom", page_Participants.prenom.getText());
+		assertEquals("Le tableau n'est pas bien affiché, la colonne ID est manquante", "ID", page_Participants.id.getText());
+		assertEquals("Le tableau n'est pas bien affiché, la colonne Code est manquante", "Code", page_Participants.code.getText());
+		assertEquals("Le tableau n'est pas bien affiché, la colonne En file est manquante", "En file", page_Participants.en_file.getText());
+		assertEquals("Le tableau n'est pas bien affiché, la colonne Opérations est manquante", "Opérations", page_Participants.operations.getText());
+		
+		//Test si le bouton créer est présent
 
 		
 		Thread.sleep(pause);
