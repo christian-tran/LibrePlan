6be6e80354bae1de34d203f28e646d8c4926069b
Arnaud <compte@gmx.com>
2020-01-07 16:04:28 +0100

    Modification Arnaud Mardi 16h04. Fin du CdT !!!

--- src/main/java/org/projet_selenium/PageRessources.java
+++ src/main/java/org/projet_selenium/PageRessources.java
@@ -27,6 +27,9 @@ public class PageRessources
 	@FindBy (xpath = "//td[@class='z-caption-l']")
 	WebElement titre_type_avancement ;
 	
+	@FindBy (xpath = "//div[contains(text(),\"Types d'avancement Liste\")]")
+	WebElement titre_type_avancement_2 ;
+	
 	@FindBy (xpath = "//span[@class='z-tab-text']")
 	WebElement tab_modif ;
 	
@@ -58,14 +61,20 @@ public class PageRessources
 	@FindBy (xpath = "//tr[@class='z-row']//input[@class='z-decimalbox']")
 	WebElement champ_val_max ;
 	
+	@FindBy (xpath = "//input[@class='z-decimalbox z-decimalbox-disd z-decimalbox-text-disd']")
+	WebElement champ_val_max_2 ;
+	
 	@FindBy (xpath = "//tr[@class='z-row z-grid-odd']//input[@class='z-decimalbox']")
 	WebElement champ_precision ;
 	
 	@FindBy (xpath = "//span[contains(text(),'User')]")
 	WebElement champ_type ;
-	
+
 	@FindBy (xpath = "//tr[6]//td[2]//div[1]//span//input")
 	WebElement checkbox_pourcentage ;
+	
+	@FindBy (xpath = "//div[@class='z-tabbox']//tr[6]//td[2]//div[1]//span[1]//input[1]")
+	WebElement checkbox_pourcentage_2 ;
 	
 	@FindBy (xpath = "//td[contains(text(),'Enregistrer')]")
 	WebElement bouton_enregistrer ;
@@ -79,33 +88,45 @@ public class PageRessources
 	@FindBy (xpath = "//div[contains(text(),\"Types d'avancement Liste\")]")
 	WebElement titre_liste_avancement ;
 	
-	@FindBy (xpath = "//div[@class='message_INFO']")
+	//Message de confirmation de creation d'avancement
+	@FindBy (xpath = "//div[@class='message_INFO']/span[@class='z-label'][contains(text(),'enregistré')]")
 	WebElement enregistrement_confirm ;
-	
+
 	//WebElements pour controle des infos dans le tableau recap des types d'avancement
 	@FindBy (xpath = "//span[contains(text(),\"Type d'avancement - Test 1\")]")
 	WebElement avancement_cree_tab ;
 	
 	@FindBy (xpath = "//div[@class='clickable-rows z-grid']//tr[6]//td[2]//div[1]//span[1]//input[1]")
-	WebElement checkbox_active ;
+	WebElement checkbox_active ; //pas bon
 	
-	@FindBy (xpath = "//tr[6]//td[3]//div[1]//span[1]//input[1]")
+	@FindBy (xpath = "//tr[6]//td[3]//div[1]//span[1]//input[1]") //pas bon
 	WebElement checkbox_predefini ;
 	
-	@FindBy (xpath = "//table//img[@src='/libreplan/common/img/ico_editar1.png']")
+	@FindBy (xpath = "//tr[6]//td[4]//div[1]//table[1]//tbody[1]//tr[1]//td[1]//table[1]//tbody[1]//tr[1]//td[1]//span[1]//table[1]//tbody[1]//tr[2]//td[2]//img[1]")
 	WebElement bouton_operations_modif ;
 	
-	@FindBy (xpath = "//table[@class='']//img[@src='/libreplan/common/img/ico_borrar1.png']")
+	@FindBy (xpath = "//tr[6]//td[4]//div[1]//table[1]//tbody[1]//tr[1]//td[1]//table[1]//tbody[1]//tr[1]//td[3]//span[1]//table[1]//tbody[1]//tr[2]//td[2]//img[1]")
 	WebElement bouton_operations_del ;
+	//
+	@FindBy (xpath = "//tr[@class='clickable-rows z-row z-grid-odd']//td[2]//div[1]//span[1]//input[1]")
+	WebElement checkbox_active_2 ; //pas bon
 	
-
+	@FindBy (xpath = "//tr[6]//td[3]//div[1]//span[1]//input[1]") //pas bon
+	WebElement checkbox_predefini_2 ;
+	
+	@FindBy (xpath = "//tr[6]//td[4]//div[1]//table[1]//tbody[1]//tr[1]//td[1]//table[1]//tbody[1]//tr[1]//td[1]//span[1]//table[1]//tbody[1]//tr[2]//td[2]//img[1]")
+	WebElement bouton_operations_modif_2 ;
+	
+	@FindBy (xpath = "//tr[6]//td[4]//div[1]//table[1]//tbody[1]//tr[1]//td[1]//table[1]//tbody[1]//tr[1]//td[3]//span[1]//table[1]//tbody[1]//tr[2]//td[2]//img[1]")
+	WebElement bouton_operations_del_2 ;
+	
+	String xpath_tableau = "//div[@class='clickable-rows z-grid']//table//tbody[@class='z-rows']/tr" ;
 	
 	
 	
 	
 	
-	
-	
+	//div[@class='clickable-rows z-grid']//tr[6]//td[2]//div[1]//span[1]//input[1]
 	
 	
 	
--- src/test/java/org/projet_selenium/testRessourcesAvancement.java
+++ src/test/java/org/projet_selenium/testRessourcesAvancement.java
@@ -8,11 +8,15 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class testRessourcesAvancement {
 
 	WebDriver driver ;
+		
+	String nom_test_avanacement = "Type d'avancement - Test 1" ;
+	String nom_test_avanacement_2 = "Type d'avancement - Test 2" ;	
 	
 	@Before
 	public void setUp() throws InterruptedException
@@ -25,14 +29,14 @@ public class testRessourcesAvancement {
 	@After
 	public void tearDown() throws InterruptedException
 	{
-		Thread.sleep(8000) ;
+		Thread.sleep(2000) ;
 		driver.quit();	
 	}
-	
 	
 	@Test
 	public void testTypeavancement() throws InterruptedException 
 	{
+		WebDriverWait wait = new WebDriverWait(driver,2) ;
 
 		//instanciation page accueil
 		PageAccueil page_Accueil = PageFactory.initElements(driver, PageAccueil.class) ;
@@ -85,7 +89,7 @@ public class testRessourcesAvancement {
 		assertTrue(page_Ressource.bouton_enregistrer.isDisplayed()) ;
 		
 		//Remplissage du tableau test 1, sans pourcentage
-		page_Ressource.champ_nom_unit.sendKeys("Type d'avancement - Test 1") ;
+		page_Ressource.champ_nom_unit.sendKeys(nom_test_avanacement) ;
 		page_Ressource.checkbox_actif.click() ;
 		OutilTechnique.remplirChamp(page_Ressource.champ_val_max, "10,00");
 		assertEquals("User",page_Ressource.champ_type.getText()) ;
@@ -95,28 +99,55 @@ public class testRessourcesAvancement {
 		
 		assertTrue(page_Ressource.titre_liste_avancement.isDisplayed()) ;
 		assertEquals("Type d'avancement \"Type d'avancement - Test 1\" enregistré", page_Ressource.enregistrement_confirm.getText()) ;
-		//driver.navigate().refresh() ;
 		assertEquals("Type d'avancement \"Type d'avancement - Test 1\" enregistré", page_Ressource.avancement_cree_tab.getText()) ;
+		
+		driver.navigate().refresh() ;
+		
 		assertFalse(page_Ressource.checkbox_active.isEnabled()) ;
 		assertFalse(page_Ressource.checkbox_predefini.isEnabled()) ;
 		assertTrue(page_Ressource.bouton_operations_modif.isDisplayed()) ;
 		assertTrue(page_Ressource.bouton_operations_del.isDisplayed()) ;
 		
-		assertTrue(page_Ressource.checkbox_active.isSelected()) ;
-		assertTrue(page_Ressource.checkbox_predefini.isSelected()) ;
+		assertFalse(page_Ressource.checkbox_active.isSelected()) ;
+		assertFalse(page_Ressource.checkbox_predefini.isSelected()) ;
 		assertTrue(page_Ressource.bouton_operations_modif.isEnabled()) ;
 		assertTrue(page_Ressource.bouton_operations_del.isEnabled()) ;
 		
+		//2 nd clique sur le bouton creer
+		page_Ressource.button_creer.click() ;
 		
+		//verif elements nécessaires à la suite des actions
+		assertTrue(page_Ressource.titre_type_avancement.getText().equals("Créer Type d'avancement")) ;//titre localisation dans la navigation
 		
+		//Remplissage du tableau
+		page_Ressource.champ_nom_unit.sendKeys(nom_test_avanacement_2) ;
+		page_Ressource.checkbox_pourcentage_2.click() ;
+
+		//Verif checkbox champ_val_max non modifiable
+		assertFalse(page_Ressource.champ_val_max_2.isEnabled()) ;
 		
+		//Clique sur sauver et continuer
+		page_Ressource.bouton_sauver_continuer.click() ;
 		
+		//Verif message et titre "Modifier Type d'avancement: Type d'avancement - Test 2"
+		assertEquals("Type d'avancement \"Type d'avancement - Test 2\" enregistré", page_Ressource.enregistrement_confirm.getText()) ;
+		assertEquals("Modifier Type d'avancement: Type d'avancement - Test 2", page_Ressource.titre_type_avancement.getText()) ;
 		
+		//Clique sur annuler puis controle de titre page en cours
+		page_Ressource.bouton_annuler.click() ;
+		driver.navigate().refresh() ;
+		assertEquals("Types d'avancement Liste", page_Ressource.titre_type_avancement_2.getText()) ;
 		
+		//controle du contenu du tableau pour test 2
+		assertFalse(page_Ressource.checkbox_active_2.isEnabled()) ;
+		assertFalse(page_Ressource.checkbox_predefini_2.isEnabled()) ;
+		assertTrue(page_Ressource.bouton_operations_modif_2.isDisplayed()) ;
+		assertTrue(page_Ressource.bouton_operations_del_2.isDisplayed()) ;
 		
-		
-		
-		
+		assertTrue(page_Ressource.checkbox_active_2.isSelected()) ;
+		assertFalse(page_Ressource.checkbox_predefini_2.isSelected()) ;
+		assertTrue(page_Ressource.bouton_operations_modif_2.isEnabled()) ;
+		assertTrue(page_Ressource.bouton_operations_del_2.isEnabled()) ;
 		
 		
 		
